6c16ad99f65fb1ece12a338eb18ae96cba9b615a
Allam9573 <aallamdaniel@gmail.com>
2020-12-23 11:10:39 -0600

    script servidor

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-var server = Express();
+const http = require('http');
+const server = http.createServer(function(request, response){
+    response.writeHead(200, {message:'Hola :)'});
+    response.write('<html><body>Hola :)</body></html>');
+    response.end();
+});
 
-function Express(){
-    console.log('Servidor conectado.');
-}
+server.listen(8080, ()=> console.log('Servidor en ejecucion...'));
