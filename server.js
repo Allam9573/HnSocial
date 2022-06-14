f69717e0331b9af3df9ec27882d5fb741b08dfb2
Allam9573 <aallamdaniel@gmail.com>
2022-06-14 15:46:23 -0600

    cambie estructura a flask

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const server = http.createServer(function(request, response){
-    response.writeHead(200, {message:'Hola :)'});
-    response.write('<html><body>Hola :)</body></html>');
-    response.end();
-});
-
-server.listen(8080, ()=> console.log('Servidor en ejecucion...'));
